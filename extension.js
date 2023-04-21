6d751ddaac6c3ec49cc3781f966dcc8cc32c8ec0
DasDarki <rageskilln@gmail.com>
2023-04-21 20:48:40 +0200

    first version with basic syntax higlighting, a bit autocompletion, compile via command and some other basic features

---
+++ extension.js
@@ -0,0 +1,51 @@
+const vscode = require('vscode');
+const cp = require('child_process');
+
+const KEYWORDS = [
+    "target", "use", "as", "router", "auth", "authenticate", "authenticated", "public", 
+    "include", "query", "header", "body", "any", "string", "float", "int", "boolean", 
+    "datetime", "time", "date", "GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"
+];
+
+function activate(context) {
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider('ymirscript', {
+            provideCompletionItems(document, position, token, context) {
+                const linePrefix = document.lineAt(position).text.substr(0, position.character);
+                if (!linePrefix.endsWith(' ')) {
+                    return undefined;
+                }
+                return KEYWORDS.map(keyword => {
+                    const completionItem = new vscode.CompletionItem(keyword, vscode.CompletionItemKind.Keyword);
+                    completionItem.insertText = keyword;
+                    return completionItem;
+                });
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerTextEditorCommand("ymirscript.compile", (textEditor, edit) => {
+            const path = textEditor.document.fileName;
+            if (!path) {
+                vscode.window.showErrorMessage("No file is opened.");
+                return;
+            }
+
+            const output = cp.spawnSync("ymir", [path]);
+            if (output.status !== 0) {
+                vscode.window.showErrorMessage(output.stderr.toString());
+                return;
+            } else {
+                vscode.window.showInformationMessage("Compile success.");
+            }
+        })
+    );
+}
+
+exports.activate = activate;
+
+module.exports = {
+    activate,
+    deactivate: () => {}
+}
